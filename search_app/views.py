dacb79805605d797ef48dd1474af3a6ade3769a8
Hannah Hall <hannahkhall13@gmail.com>
2017-02-07 14:55:20 -0600

    views and urls for index and search

--- search_app/views.py
+++ search_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic.base import TemplateView
+import json
+from v1 import guidebox
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'search_app/index.html'
+
+def search(request):
+    name = request.POST['movie_name']
+    movie_list = guidebox.Search().movie_search_by_title(
+        field='title',
+        query=name
+    )
+    return render(request, 'search_app/movie_list.html', {'movie_list': movie_list})
