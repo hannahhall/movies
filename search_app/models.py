1f8fa5250e0c09d38b0a5e0363af99a2c035e5e8
Hannah Hall <hannahkhall13@gmail.com>
2017-02-07 13:12:13 -0600

    migrations

--- search_app/models.py
+++ search_app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Movie(models.Model):
+    g_id = models.IntegerField(primary_key=True)
+    original_name = models.CharField(max_length=200)
+    img_url = models.URLField()
+
+
 class UserExtend(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     saved_movies = models.ManyToManyField(Movie, related_name='users')
-
-
-class Movie(models.Model):
-    pk = models.IntegerField(primary_key=True)
-    original_name = models.CharField(max_length=None)
-    img_url = models.URLField()
