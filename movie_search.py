7630f789050be51ad7ed561293ede5bb8163e6e5
Hannah Hall <hannahkhall13@gmail.com>
2017-02-06 19:56:29 -0600

    basic Search by title or id

---
+++ movie_search.py
@@ -0,0 +1,24 @@
+import guidebox
+from api_key import key
+
+
+
+class Search:
+
+    guidebox.api_key = key
+
+    def movie_search_by_title (self, field, query):
+        """Search for movies by title"""
+        results = guidebox.Search.movies(field=field, query=query)
+        return results.results
+
+    def movie_lookup_by_id(self, movie_id):
+        """Look up specific movie by id"""
+        result = guidebox.Movie.retrieve(id=movie_id)
+        return result
+
+#choose movie from Search
+
+
+#list source URLs
+    #if can't find subscription sources look in paid
