16f08a96df0ce77e5e398b028015eea08444aa95
Hannah Hall <hannahkhall13@gmail.com>
2017-02-14 18:21:29 -0600

    url fix

--- search_app/urls.py
+++ search_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'search_app'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^movie-search/$', views.search, name='movie_search'),
+    url(r'^movie-search/$', views.search_by_title, name='movie_search'),
     url(r'^movie/(?P<movie_id>[0-9]+)/', views.movie_detail, name='detail'),
-    url(r'^actor/(?P<actor_id>[0-9]+)/', views.actor_detail, name='actor_detail')
+    url(r'^actor/(?P<actor_id>[0-9]+)/', views.actor_detail, name='actor_detail'),
     url(r'^actor-search/$', views.search_by_actor, name='actor_search')
 ]
